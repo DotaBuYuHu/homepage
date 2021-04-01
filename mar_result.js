b15b1a4296748e59d0162d7d44a64d23b7b0f502
Brian Liu <bliu1@godaddy.com>
2021-04-01 11:22:45 -0700

    april update

---
+++ mar_result.js
@@ -0,0 +1,263 @@
+var mar_result = [
+    {
+        "name": "主席",
+        "win": 2,
+        "lose": 0,
+        "total": 2,
+        "percentage": 100,
+        "point": 4,
+        "$$hashKey": "object:49"
+    },
+    {
+        "name": "Autismy",
+        "win": 17,
+        "lose": 28,
+        "total": 45,
+        "percentage": 37,
+        "point": 6,
+        "$$hashKey": "object:50"
+    },
+    {
+        "name": "乐章",
+        "win": 3,
+        "lose": 4,
+        "total": 7,
+        "percentage": 42,
+        "point": 2,
+        "$$hashKey": "object:51"
+    },
+    {
+        "name": "扬神",
+        "win": 11,
+        "lose": 12,
+        "total": 23,
+        "percentage": 47,
+        "point": 10,
+        "$$hashKey": "object:52"
+    },
+    {
+        "name": "bottlezz2002",
+        "win": 10,
+        "lose": 9,
+        "total": 19,
+        "percentage": 52,
+        "point": 11,
+        "$$hashKey": "object:53"
+    },
+    {
+        "name": "赵德柱",
+        "win": 22,
+        "lose": 15,
+        "total": 37,
+        "percentage": 59,
+        "point": 29,
+        "$$hashKey": "object:54"
+    },
+    {
+        "name": "木暮的执着",
+        "win": 9,
+        "lose": 11,
+        "total": 20,
+        "percentage": 45,
+        "point": 7,
+        "$$hashKey": "object:55"
+    },
+    {
+        "name": "演员已就位",
+        "win": 9,
+        "lose": 9,
+        "total": 18,
+        "percentage": 50,
+        "point": 9,
+        "$$hashKey": "object:56"
+    },
+    {
+        "name": "Aren",
+        "win": 31,
+        "lose": 22,
+        "total": 53,
+        "percentage": 58,
+        "point": 40,
+        "$$hashKey": "object:57"
+    },
+    {
+        "name": "element蛙呱呱",
+        "win": 11,
+        "lose": 16,
+        "total": 27,
+        "percentage": 40,
+        "point": 6,
+        "$$hashKey": "object:58"
+    },
+    {
+        "name": "Wolfpack",
+        "win": 12,
+        "lose": 17,
+        "total": 29,
+        "percentage": 41,
+        "point": 7,
+        "$$hashKey": "object:59"
+    },
+    {
+        "name": "Voronoi",
+        "win": 12,
+        "lose": 6,
+        "total": 18,
+        "percentage": 66,
+        "point": 18,
+        "$$hashKey": "object:60"
+    },
+    {
+        "name": "jennierubyjane",
+        "win": 26,
+        "lose": 21,
+        "total": 47,
+        "percentage": 55,
+        "point": 31,
+        "$$hashKey": "object:61"
+    },
+    {
+        "name": "送僵",
+        "win": 20,
+        "lose": 17,
+        "total": 37,
+        "percentage": 54,
+        "point": 23,
+        "$$hashKey": "object:62"
+    },
+    {
+        "name": "幸福啪啪啪",
+        "win": 4,
+        "lose": 5,
+        "total": 9,
+        "percentage": 44,
+        "point": 3,
+        "$$hashKey": "object:63"
+    },
+    {
+        "name": "3700",
+        "win": 1,
+        "lose": 5,
+        "total": 6,
+        "percentage": 16,
+        "point": -3,
+        "$$hashKey": "object:64"
+    },
+    {
+        "name": "Cilymice",
+        "win": 11,
+        "lose": 13,
+        "total": 24,
+        "percentage": 45,
+        "point": 9,
+        "$$hashKey": "object:65"
+    },
+    {
+        "name": "Tang",
+        "win": 11,
+        "lose": 11,
+        "total": 22,
+        "percentage": 50,
+        "point": 11,
+        "$$hashKey": "object:66"
+    },
+    {
+        "name": "全能莽夫戴维德",
+        "win": 12,
+        "lose": 13,
+        "total": 25,
+        "percentage": 48,
+        "point": 11,
+        "$$hashKey": "object:67"
+    },
+    {
+        "name": "ppp",
+        "win": 6,
+        "lose": 4,
+        "total": 10,
+        "percentage": 60,
+        "point": 8,
+        "$$hashKey": "object:68"
+    },
+    {
+        "name": "地鼠",
+        "win": 10,
+        "lose": 10,
+        "total": 20,
+        "percentage": 50,
+        "point": 10,
+        "$$hashKey": "object:69"
+    },
+    {
+        "name": "朴素",
+        "win": 10,
+        "lose": 10,
+        "total": 20,
+        "percentage": 50,
+        "point": 10,
+        "$$hashKey": "object:70"
+    },
+    {
+        "name": "Grandson Liu",
+        "win": 1,
+        "lose": 2,
+        "total": 3,
+        "percentage": 33,
+        "point": 0,
+        "$$hashKey": "object:71"
+    },
+    {
+        "name": "Leo",
+        "win": 7,
+        "lose": 4,
+        "total": 11,
+        "percentage": 63,
+        "point": 10,
+        "$$hashKey": "object:72"
+    },
+    {
+        "name": "九中街车神",
+        "win": 5,
+        "lose": 4,
+        "total": 9,
+        "percentage": 55,
+        "point": 6,
+        "$$hashKey": "object:73"
+    },
+    {
+        "name": "ACE",
+        "win": 1,
+        "lose": 4,
+        "total": 5,
+        "percentage": 20,
+        "point": -2,
+        "$$hashKey": "object:74"
+    },
+    {
+        "name": "V",
+        "win": 3,
+        "lose": 6,
+        "total": 9,
+        "percentage": 33,
+        "point": 0,
+        "$$hashKey": "object:75"
+    },
+    {
+        "name": "Fabric",
+        "win": 1,
+        "lose": 0,
+        "total": 1,
+        "percentage": 100,
+        "point": 2,
+        "$$hashKey": "object:76"
+    },
+    {
+        "name": "Lala",
+        "win": 7,
+        "lose": 7,
+        "total": 14,
+        "percentage": 50,
+        "point": 7,
+        "$$hashKey": "object:77"
+    }
+]
